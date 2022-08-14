86f2adccb0af00fec18ac9c66e9708d51e0322cc
Mateus Auler <mateusauler@protonmail.com>
2022-08-14 04:49:42 -0300

    create-shortcuts: Fix typo in the shortcut list filename

--- scripts/create-shortcuts.sh
+++ scripts/create-shortcuts.sh
@@ -5,7 +5,7 @@ origin=$(pwd)
 pushd $HOME/.scripts
 chmod +x *.sh
 
-cat $origin/res/shorcuts | while read line ; do
+cat $origin/res/shortcuts | while read line ; do
 	add_shortcut.sh $line
 done
 
